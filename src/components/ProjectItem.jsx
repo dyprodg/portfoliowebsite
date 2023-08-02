dbb49f6948d2df673b9776a4244eea6bae41ddef
dyprodg <dennisdiepolder@outlook.com>
2023-08-02 19:05:36 +0200

    bearson

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-
-const ProjectItem = ({ img, title }) => {
+const ProjectItem = ({ img, title , link}) => {
   return (
     <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-black'>
-      <img src={img} alt="/" className='rounded-xl group-hover:opacity-10'/>
-      <div className='hidden group-hover:block absolute top-[50%] translate-x-[-50%] translate-y-[-50%]'>
-        <h3 className='text-2xl font-bold text-white tracking-wider text-center'>
-          {title}
-        </h3>
-        <p className='pb-4 pt-2 text-white text-center'>React Js</p>
-        <a>
-          <p className='text-center p-3 rounded-ld bg-white text-gray-700 font-bold curser-pointer text-lg hover:scale-110 ease-in duration-150'>More Info</p>
-        </a>
-      </div>
+     
+      <img src={img} alt="/" className='rounded-xl group-hover:opacity-10 h-[300px] w-full object-cover'/>
+
+      <div className='hidden group-hover:flex flex-col items-center justify-center absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+  <h3 className='text-2xl font-bold text-white tracking-wider text-center'>
+    {title}
+  </h3>
+  <p className='pb-4 pt-2 text-white text-center'>React Js</p>
+  <a>
+  <p onClick={() => window.open(link, '_blank')}
+   className='text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg w-[150px] hover:scale-110 ease-in duration-150'>
+  More Info
+</p>
+  </a>
+</div>
     </div>
   );
 };
