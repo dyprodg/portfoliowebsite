37df293acb22cecd0f1679f49cdd3b470d22555d
dyprodg <dennisdiepolder@outlook.com>
2023-09-05 17:10:00 +0200

    workchanges

--- src/components/Impressum.jsx
+++ src/components/Impressum.jsx
@@ -3,6 +3,22 @@ import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-k
 
 const Impressum = () => {
   return (
+<div className="flex flex-wrap px-10">
+      <div className="w-full sm:w-1/2 md:w-1/2 p-4 text-xs">
+        <h2 className="text-lg font-semibold mb-4">Personal Information</h2>
+        <p>Name: Dennis Diepolder</p>
+        <p>City: 82279 Eching</p>
+        <p>Phone: +4916091208750</p>
+        <p>Email: dennisdiepolder@outlook.com</p>
+      </div>
+      <div className="w-full sm:w-1/2 md:w-1/3 p-4 text-xs">
+        <h2 className="text-lg font-semibold mb-4">Legal Information</h2>
+        <p>This website only uses technically necessary cookies. These cookies are essential for the operation of the website and do not store any personally identifiable information. By continuing to browse the site, you agree to the use of these cookies. The website collects no personal data.</p>
+
+      </div>
+    </div>
+    
+    /*
     <MDBFooter bgColor='light' className='max-w-[1040px] m-auto pl-0 justify-center text-center text-lg-start text-muted'>
       <MDBContainer>
         <MDBRow className='mt-3'>
@@ -31,6 +47,7 @@ const Impressum = () => {
         <p className="mb-0">All rights reserved</p>
       </div>
     </MDBFooter>
+    */
   );
 }
 
