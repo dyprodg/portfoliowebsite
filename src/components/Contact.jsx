03d16b2d1d956805beeaf00c1d391d06618b231b
dyprodg <dennisdiepolder@outlook.com>
2023-09-07 16:21:37 +0200

    contact layout

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,8 +5,8 @@ const Contact = () => {
   return (
     <div id='contact' className='max-w-[1040] m-auto md:pl-20 p-4'>
         <h1 className='py-4 text-4xl font-bold text-center text-black shadow-gray-500'>Contact</h1>
-        <h1 className='py-10 text-5xl font-bold text-center text-black'>If you wish to get in touch with me, please feel free to reach out through the following social media platforms listed below.</h1>
-        <div className='grid'>
+        <h1 className='py-10 text-2xl font-bold text-center text-black'>If you wish to get in touch with me, please feel free to reach out through the following social media platforms listed below.</h1>
+     
         <div className='flex m-auto pl-10 md:pl-0 py-28 max-w-[1040px] justify-center w-full'>
             <FaTwitter
               onClick={() => window.open('https://twitter.com/dyprodg', '_blank')}
@@ -27,9 +27,32 @@ const Contact = () => {
               onClick={() => window.open('https://www.linkedin.com/in/dennis-diepolder-2a5a98276/', '_blank')}
               className='cursor-pointer hover:scale-110 ease-in duration-200 mr-8'
               size={100} />
+        </div>
+        <form action="" method='POST' encType='multipart/from-data'>
+          <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+            <div className='flex flex-col'>
+              <label className='uppercase text-sm py-2'>Name</label>
+              <input className='border-2 rounded-lg p-3 flex border-gray-300 shadow-md hover:shadow-lg transition-shadow' type='text' name='name' />
             </div>
-      
+            <div className='flex flex-col'>
+              <label className='uppercase text-sm py-2'>Phone Number</label>
+              <input className='border-2 rounded-lg p-3 flex border-gray-300 shadow-md hover:shadow-lg transition-shadow' type='text' name='phone' />
             </div>
+          </div>
+          <div className='flex flex-col'>
+            <label className='uppercase text-sm py-2'>Email</label>
+            <input className='border-2 rounded-lg p-3 flex border-gray-300 shadow-md hover:shadow-lg transition-shadow' type='email' name='email'/>
+          </div>
+          <div className='flex flex-col'>
+            <labe className='uppercase text-sm py-2'>Subject</labe>
+            <input className='border-2 rounded-lg p-3 flex border-gray-300 shadow-md hover:shadow-lg transition-shadow' type='text' name='subject'/>
+          </div>
+          <div className='flex flex-col'>
+            <label className='uppercase text-sm py-2'>Message</label>
+            <textarea className='border-2 rounded-lg p-3 flex border-gray-300 shadow-md hover:shadow-lg transition-shadow' rows='10' name='message'/>
+          </div>
+          
+        </form>
     </div>
   )
 }
