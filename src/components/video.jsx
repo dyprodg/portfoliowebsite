203c7df24331bd8887a1917d7f585ca3c522f0fe
dyprodg <dennisdiepolder@outlook.com>
2023-11-30 13:59:18 +0100

    videotest2

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -1,41 +1,11 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 
-const AutoPlayVideo = () => {
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    const options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.5,
-    };
-
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-
-          videoRef.current.play();
-        } else {
-
-          videoRef.current.pause();
-        }
-      });
-    }, options);
-
-    observer.observe(videoRef.current);
-
-
-    return () => observer.disconnect();
-  }, []);
+const VideoPlayer = () => {
+  const videoUrl = 's3://dennis-video-portfolio/bewerbungsvideo-final.mp4';
 
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <video
-        ref={videoRef}
-        controls
-        width="640"
-        height="360"
-      >
+    <div className='flex w-full justify-center items-center'>
+      <video className='m-4 max-h-[600]' controls width="640" height="360">
         <source src={videoUrl} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
@@ -43,4 +13,4 @@ const AutoPlayVideo = () => {
   );
 };
 
-export default AutoPlayVideo;
+export default VideoPlayer;
